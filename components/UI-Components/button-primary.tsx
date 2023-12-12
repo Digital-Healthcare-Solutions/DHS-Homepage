71430fd5935f8b4de6ba807991dfb16cf5c878a1
Bucky <jacobowens75@gmail.com>
2023-12-11 23:08:03 -0600

    added optional onClick to button primary

--- components/UI-Components/button-primary.tsx
+++ components/UI-Components/button-primary.tsx
@@ -1,12 +1,13 @@
-const ButtonPrimary = ({
-    children,
-    onClick = () => {},
-    className,
-    ...props
-}) => {
+const ButtonPrimary = ({ children, onClick, className, ...props }) => {
     return (
         <button
-            onClick={onClick}
+            onClick={
+                onClick
+                    ? onClick
+                    : () => {
+                          console.log("")
+                      }
+            }
             {...props}
             className={`bg-blue-500 hover:bg-white hover:ring-1 ring-blue-500 text-white hover:text-blue-500 font-bold py-2 px-4 rounded-lg shadow-md font-sans flex items-center active:bg-white active:text-blue-500 active:ring-1 active:ring-blue-500
       ${className}`}
