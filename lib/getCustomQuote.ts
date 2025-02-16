c4004550648529770cd19bf2963911271c36baae
Jacob <jacobowens75@gmail.com>
2025-02-15 20:00:28 -0600

    fix:edited api urls

--- lib/getCustomQuote.ts
+++ lib/getCustomQuote.ts
@@ -1,39 +1,39 @@
 export const getCustomQuote = async ({
-    name,
-    email,
-    business,
-    providers,
-    features
+  name,
+  email,
+  business,
+  providers,
+  features
 }: {
-    name: string
-    email: string
-    business: string
-    providers: number
-    features: string[]
+  name: string
+  email: string
+  business: string
+  providers: number
+  features: string[]
 }) => {
-    try {
-        const res = await fetch(
-            "https://api.digitalhealthcaresolutions.io/api:5iYyLrKQ/customQuote",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    name,
-                    email,
-                    business,
-                    providers,
-                    features
-                })
-            }
-        )
-        const data = await res.json()
-        if (!res.ok) {
-            throw new Error(data.message || "An error occurred")
-        }
-        return data
-    } catch (error: any) {
-        throw new Error(error)
+  try {
+    const res = await fetch(
+      "https://api.digitalhealthcaresolutions.io/api:5iYyLrKQ/custom-quote",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          business,
+          providers,
+          features
+        })
+      }
+    )
+    const data = await res.json()
+    if (!res.ok) {
+      throw new Error(data.message || "An error occurred")
     }
+    return data
+  } catch (error: any) {
+    throw new Error(error)
+  }
 }
--- lib/requestDemo.ts
+++ lib/requestDemo.ts
@@ -1,33 +1,33 @@
 export const requestDemo = async ({
-    name,
-    email,
-    business
+  name,
+  email,
+  business
 }: {
-    name: string
-    email: string
-    business: string
+  name: string
+  email: string
+  business: string
 }) => {
-    try {
-        const res = await fetch(
-            "https://xmks-s250-ypw0.n7.xano.io/api:5iYyLrKQ/subscribeToLaunchList",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    name,
-                    email,
-                    business
-                })
-            }
-        )
-        const data = await res.json()
-        if (!res.ok) {
-            throw new Error(data.message || "An error occurred")
-        }
-        return data
-    } catch (error: any) {
-        throw new Error(error)
+  try {
+    const res = await fetch(
+      "https://api.digitalhealthcaresolutions.io/api:5iYyLrKQ/request-demo",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          business
+        })
+      }
+    )
+    const data = await res.json()
+    if (!res.ok) {
+      throw new Error(data.message || "An error occurred")
     }
+    return data
+  } catch (error: any) {
+    throw new Error(error)
+  }
 }
