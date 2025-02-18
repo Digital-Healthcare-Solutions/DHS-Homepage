6ea830cd10ffd943cf11a80f4152fc67071c2058
Bucky <jacobowens75@gmail.com>
2025-02-17 20:19:11 -0600

    added posthog web analytics

--- components/client/providers/ProviderWrapper.tsx
+++ components/client/providers/ProviderWrapper.tsx
@@ -5,71 +5,83 @@ import React, { useEffect } from "react"
 import { ThemeProvider } from "./theme-provider"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { Intercom, shutdown } from "@intercom/messenger-js-sdk"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
+import SuspendedPostHogPageView from "../misc/post-hog-page-view"
 
 const ProviderWrapper = ({ children }: { children: React.ReactNode }) => {
-    //Intercom
-    useEffect(() => {
-        Intercom({
-            app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID!,
-            hide_default_launcher: false
-        })
+  //Intercom
+  useEffect(() => {
+    Intercom({
+      app_id: process.env.NEXT_PUBLIC_INTERCOM_APP_ID!,
+      hide_default_launcher: false
+    })
 
-        return () => {
-            shutdown()
-        }
-    }, [])
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      capture_pageleave: true
+    })
 
-    const queryClient = new QueryClient()
+    return () => {
+      shutdown()
+    }
+  }, [])
 
-    return (
-        <div>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
+  const queryClient = new QueryClient()
+
+  return (
+    <div>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
         `}
-            </Script>
-            {/* <!-- Google tag (gtag.js) --> */}
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}`}
-            ></Script>
-            <Script>
-                {` window.dataLayer = window.dataLayer || [];
+      </Script>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}`}
+      ></Script>
+      <Script>
+        {` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}');`}
-            </Script>
-            {/* <!-- Google tag (gtag.js) --> */}
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}`}
-            ></Script>
-            <Script>
-                {` window.dataLayer = window.dataLayer || [];
+      </Script>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}`}
+      ></Script>
+      <Script>
+        {` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID2}');`}
-            </Script>
-            <QueryClientProvider client={queryClient}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    {children}
-                </ThemeProvider>
-            </QueryClientProvider>
-        </div>
-    )
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <PostHogProvider client={posthog}>
+          <SuspendedPostHogPageView />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </PostHogProvider>
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default ProviderWrapper
